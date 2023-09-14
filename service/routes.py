75a9a610813f03b3d846315f54ba225407d420e1
sylviewn <sylviewetten@gmail.com>
2023-09-14 15:50:22 -0400

    commit Update an account in the service

--- service/routes.py
+++ service/routes.py
@@ -86,7 +86,7 @@ def get_accounts(account_id):
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
-    Update an Account
+    Update an Account in the service
     This endpoint will update an Account based on the posted data
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
